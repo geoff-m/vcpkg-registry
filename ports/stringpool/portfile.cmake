12cb087d0fd97765729438b615d6d748544bc255
Geoff McQueen <geoff@geoff.space>
2026-04-29 03:02:49 -0700

    Fix SHA

--- ports/stringpool/portfile.cmake
+++ ports/stringpool/portfile.cmake
@@ -2,7 +2,7 @@ vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO geoff-m/stringpool
     REF v1.0.0
-    SHA512 0
+    SHA512 b63976edd27169fdef59ebb581a3ba8901d241cabb461ba0c82fbef3d9c34adfca7cc6bc2430d53e4014ee3d50180069e209d5d516889be22730ea37c34dd880
     HEAD_REF master
 )
 
