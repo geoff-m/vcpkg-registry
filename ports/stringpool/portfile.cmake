7d14e203b49a1d718c4a2814bd1e93f9a2d383dd
Geoff McQueen <geoff@geoff.space>
2026-07-14 02:45:07 -0700

    Update portfile for stringpool

--- ports/stringpool/portfile.cmake
+++ ports/stringpool/portfile.cmake
@@ -1,9 +1,9 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO geoff-m/stringpool
-    REF 3da9aba0fedf7c1ab6fdc776c42d289611b11f80
-    SHA512 9e9633c375f2bcd2b076cdb5b7271a20bbc8ba084ab2a344bedb29cc409b3726c80da7e6adbdd9679a0fc0373c416d8fdb86f8fd6191e7948d0ce461a429684f
-    HEAD_REF feature/package
+    REF 919c2914252128fc6951aeed6ef5b2748b961380
+    SHA512 0
+    HEAD_REF master
 )
 
 vcpkg_cmake_configure(SOURCE_PATH "${SOURCE_PATH}"
